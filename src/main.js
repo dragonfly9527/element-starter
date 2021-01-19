b793b5e63a4bfc53bfe748ac8d08df68d90cb4dc
dragonfly9527 <tg253256958@163.com>
2021-01-19 14:11:13 +0800

    测试提交

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
 Vue.use(ElementUI)
+// 全局调整size和zIndex
+//Vue.use(Element, { size: 'small', zIndex: 3000 });
 
 new Vue({
   el: '#app',
